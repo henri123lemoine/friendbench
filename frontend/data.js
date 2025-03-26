61dac5fe5919989f897474b131e86c518b4e3531
Henri Lemoine <henri123lemoine@gmail.com>
2025-03-25 21:27:00 -0400

    Added Gemini 2.5 to friendbench

--- frontend/data.js
+++ frontend/data.js
@@ -2,14 +2,14 @@
 const modelData = [
   { name: "Sonnet 3.7", score: 64 },
   { name: "Sonnet 3.6", score: 78 },
-  { name: "Sonnet 3.5", score: 72 },
   { name: "Opus 3", score: 65 },
   { name: "Grok 3", score: 51 },
   { name: "GPT-4.5", score: 69 },
   { name: "GPT-4o\n(old)", score: 34 },
   { name: "GPT-4o\n(latest)", score: 47 },
   { name: "o3-mini\n(high)", score: 26 },
-  { name: "R1", score: 22 }
+  { name: "DeepSeek-R1", score: 22 },
+  { name: "Gemini\n2.5 Pro", score: 57 },
 ];
 
 // Quotes data
@@ -18,33 +18,33 @@ const quotesData = [
     text: "If Claude was a human it would be the best human",
     author: "Zainab",
     models: ["Sonnet 3.6"],
-    showModelInline: true
+    showModelInline: true,
   },
   {
     text: "Grok also gives big model smell and has become my daily driver model, also a pretty pleasant personality, definitely definitely worth trying - just a bit immature/overly glossy vibes sometimes, whereas 4.5 is much more real",
     author: "Andrew",
-    models: ["Grok 3", "GPT-4.5"]
+    models: ["Grok 3", "GPT-4.5"],
   },
   {
     text: "4.5 is very very friend",
     author: "[redacted]",
-    models: ["GPT-4.5"]
+    models: ["GPT-4.5"],
   },
   {
     text: "yeah xai cooked on this one",
     author: "Thomas",
     models: ["Grok 3"],
-    showModelInline: true
+    showModelInline: true,
   },
   {
     text: "Grok 3 is good. I change my mind about GPT-4.5. Kinda mid. Claude 3.6 still wins on FriendBench, even above 3.7",
     author: "Henri",
-    models: ["Grok 3", "GPT-4.5", "Sonnet 3.6", "Sonnet 3.7"]
+    models: ["Grok 3", "GPT-4.5", "Sonnet 3.6", "Sonnet 3.7"],
   },
   {
     text: "yeah I am surprisingly not enchanted with 3.7",
     author: "Andrew",
-    models: ["Sonnet 3.7"]
+    models: ["Sonnet 3.7"],
   },
   // {
   //   text: "I think the post-training may have been rushed on this one",
