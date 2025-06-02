9621e0788e18e9d9027b2b9030e6186efb404014
Henri Lemoine <henri123lemoine@gmail.com>
2025-06-02 14:25:01 -0700

    Updated data

--- frontend/data.js
+++ frontend/data.js
@@ -1,5 +1,7 @@
 // FriendBench model data
 const modelData = [
+  { name: "Sonnet 4", score: 78 },
+  { name: "Opus 4", score: 80 },
   { name: "Sonnet 3.7", score: 64 },
   { name: "Sonnet 3.6", score: 78 },
   { name: "Opus 3", score: 65 },
@@ -64,3 +66,69 @@ const quotesData = [
   //   showModelInline: true
   // }
 ];
+
+/*
+# Anthropic
+claude-3-7-sonnet-20250219
+claude-3-5-sonnet-20241022
+claude-3-5-haiku-20241022
+claude-3-5-sonnet-20240620
+claude-3-haiku-20240307
+claude-3-opus-20240229
+claude-3-sonnet-20240229
+claude-2.1
+claude-2.0
+
+
+# OpenAI
+## GPT-4o
+gpt-4o-2024-11-20
+gpt-4o-audio-preview
+gpt-4o-2024-05-13
+gpt-4o-search-preview-2025-03-11
+gpt-4o-search-preview
+gpt-4o-realtime-preview-2024-12-17
+gpt-4o-realtime-preview
+gpt-4o-transcribe
+gpt-4o-2024-08-06
+gpt-4o-realtime-preview-2024-10-01
+gpt-4o-audio-preview-2024-12-17
+gpt-4o-audio-preview-2024-10-01
+## GPT-4o-mini
+gpt-4o-mini-transcribe
+gpt-4o-mini-2024-07-18
+gpt-4o-mini
+gpt-4o-mini-search-preview
+gpt-4o-mini-search-preview-2025-03-11
+gpt-4o-mini-tts
+gpt-4o-mini-realtime-preview-2024-12-17
+gpt-4o-mini-audio-preview-2024-12-17
+gpt-4o-mini-realtime-preview
+gpt-4o-mini-audio-preview
+## 4.5
+gpt-4.5-preview
+gpt-4.5-preview-2025-02-27
+## o-model
+o3-mini-2025-01-31
+o3-2025-04-16
+o4-mini-2025-04-16
+o1-2024-12-17
+o3-mini
+o1-mini
+o1-mini-2024-09-12
+o3
+o4-mini
+o1-pro-2025-03-19
+o1
+o1-pro
+o1-preview
+o1-preview-2024-09-12
+
+# DeepSeek
+DeepSeek-R1
+Deepseek-R1 (new)
+
+# Google Deepmind
+Gemini 2.5 Pro
+Gemini 2.0 Flash
+*/
